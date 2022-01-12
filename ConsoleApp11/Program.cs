bea162ee6f41d10be96464be067eab4f8a88b91c
AntonTevseev <91051811+AntonTevseev@users.noreply.github.com>
2022-01-12 22:37:35 +0200

    Create .dtbcache.v2

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using System.IO;
+
+namespace xuy
+{
+    class Program
+    {
+
+        static void Main(string[] ages)
+        {
+           
+            StreamWriter sw = new StreamWriter("new.txt",true);
+
+            string key; 
+
+            string note = string.Empty;
+            
+            while(true)
+            {
+                Write("\nВведите ID ");
+                note += $"{ReadLine()}\t";
+
+                Write("\nВведите Ф И О ");
+                note += $"{ReadLine()}\t";
+
+                Write("\nВведите ваш возраст");
+                note += $"{ReadLine()}\t";
+
+                Write("\nВведите ваш рост ");
+                note += $"{ReadLine()}\t";
+
+                Write("\nВведите ваш возраст ");
+                note += $"{ReadLine()}\t";
+
+                Write("\nВведите ваше место рождения ");
+                note += $"{ReadLine()}\t";
+
+                string now = DateTime.Now.ToShortTimeString();
+                note += $"{now}\t";
+
+                sw.Write(note);
+                Write("Если хотите вывести информацию на экран нажмите 1, если хотите продолжить нажмите 2");
+                key = ReadLine();
+
+                if (key == "1") { Write(note); break; }
+                else { }
+            }
+            
+            
+
+        }
+    }
+}
